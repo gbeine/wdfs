490312a467e0626161389f87557ba1e95e37047d
Gerrit Beine <gerrit.beine@gmx.de>
2014-09-03 17:58:02 +0200

    Import of wdfs-1.3.1.tar.gz
    Released 2006-08-01
    
    no need to upgrade from v1.3.0, because i just added a line to the licence statement for debian legal compatibility.

--- src/wdfs-main.c
+++ src/wdfs-main.c
@@ -19,6 +19,9 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ *  This program is released under the GPL with the additional exemption
+ *  that compiling, linking and/or using OpenSSL is allowed.
  */
 
 #include <stdio.h>
